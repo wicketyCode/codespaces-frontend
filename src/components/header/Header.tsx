17d67868a96e93d1347ab64eddedcd40112d8616
Erol Eskici <aberec@almbrand.dk>
2022-12-26 12:04:58 +0100

    header fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -76,15 +76,13 @@ export default function Header() {
                 <ProfileMenu />
               </div>
             </div>
-          </div>
-
+          </div>             
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pt-2 pb-3">
               {navigation.map((item) => (
-                <Disclosure.Button
+                <Link
                   key={item.name}
-                  as="a"
-                  href={item.href}
+                  to={item.href}
                   className={classNames(
                     item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
                     "block px-3 py-2 rounded-md text-base font-medium"
@@ -92,7 +90,7 @@ export default function Header() {
                   aria-current={item.current ? "page" : undefined}
                 >
                   {item.name}
-                </Disclosure.Button>
+                </Link>
               ))}
             </div>
           </Disclosure.Panel>
