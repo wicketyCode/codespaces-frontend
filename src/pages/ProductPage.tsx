d62ef0eabfa95ab29bbe1759f3a14659f3a2586d
Erol Eskici <aberec@almbrand.dk>
2022-12-29 20:45:50 +0100

    props work

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -29,7 +29,7 @@ function ContactPage() {
   return (
     <>
       <div className="text-center">
-        <h1 className="text-2xl font-bold">Welcome to the ContactPage!</h1>
+        <h1 className="text-2xl font-bold">T-shirts & polos for men</h1>
         <p className="text-gray-700">This is the ContactPage of our application.</p>
       </div>
       <div className="mt-6 grid justify-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-8">
